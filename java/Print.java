b1ffcd1767a7d95ec97fc91e31180bc2f15856f4
oliversimonics-page <oliversimonics@gmail.com>
2025-10-01 17:02:34 +0200

    Delete java directory

--- java/Print.java
+++
@@ -1,9 +0,0 @@
-public class Print {
-    public static void main(String[] args) {
-        int i;
-        for(i = 1; i <= 4; i++) {
-            System.out.println( (float)i /2);
-        }
-        System.out.println("I displayed " + (i-1) + " numbers");
-    }
-}
